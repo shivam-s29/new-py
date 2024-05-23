7ca00c2b056c4e9c3653f87d29fad9f6bbe65272
Ubuntu <ubuntu@ip-172-31-19-238.us-west-1.compute.internal>
2024-05-23 08:51:56 +0000

    1

---
+++ userdata.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt-get update -y
+sudo apt-get install -y python3 python3-pip git
+git clone https://github.com/Hemayuva/USA-Housing.git /home/ubuntu/USA-housing
+cd /home/ubuntu/USA-housing
+pip3 install -r requirements.txt
+python3 app.py
+screen -m python3 app.py
+screen -m -d python3 app.py
